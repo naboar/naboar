d909a192dbf5f65817862f41d3c7667c7a2b6719
Anthony Freda <anthonyfreda323@gmail.com>
2019-01-08 13:29:22 -0500

    updated app

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,34 +1,13 @@
 import React, { Component } from 'react'
-import { Button, Expand } from '../../src'
+import { Button } from '../../src'
 
 const noop = () => null
 
 class App extends Component {
-  state = {
-    expand: false,
-  }
-  expand = () => {
-    this.setState({ expand: !this.state.expand })
-  }
   render() {
     return (
       <div>
-        <Expand isExpanded={this.state.expand} from={0} to={100}>
-          <div style={{ height: 100, background: 'black', color: 'white' }}>
-            <h1>Hello</h1>
-          </div>
-        </Expand>
-        <Expand
-          vertical={true}
-          isExpanded={this.state.expand}
-          from={0}
-          to={100}
-        >
-          <div style={{ height: 100, background: 'black', color: 'white' }}>
-            <h1>Hello</h1>
-          </div>
-        </Expand>
-        <Button onClick={this.expand}>Click</Button>
+        <Button onClick={alert}>Click Me</Button>
       </div>
     )
   }
