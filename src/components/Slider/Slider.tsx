501a9e12422daa89a23e5e6ace71f92e2046d800
Anthony Freda <anthonyfreda323@gmail.com>
2019-01-08 17:32:56 -0500

    init component

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+/**
+ * Slider Component
+ * @since v1.0.0
+ * @author [Anthony Freda](https://github.com/Afreda323)
+ */
+const Slider = (props: ISliderProps) => {
+  return <div />
+}
+
+/**
+ * Slider prop interface
+ */
+interface ISliderProps {
+    foo: string
+}
+
+export default Slider
