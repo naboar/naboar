f46a91a8c80ec771ff5c38539b4a3481960a3dc6
Anthony Freda <anthonyfreda323@gmail.com>
2019-01-11 13:42:44 -0500

    init pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+/**
+ * Pagination Component
+ *
+ * @since v1.0.0
+ * @author [Anthony Freda](https://github.com/Afreda323)
+ */
+const Pagination = () => <Wrapper>Pagination</Wrapper>
+
+// Types -----
+/** Pagination props interface */
+
+// Styled Components -----
+
+const Wrapper = styled.div`
+  display: inline-block;
+`
+
+export default Pagination
