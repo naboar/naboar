b0fb0aa348cab171b00e011eb5c08df4f019aceb
Anthony Freda <anthonyfreda323@gmail.com>
2019-01-11 18:50:33 -0500

    init datepicker

---
+++ src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+/**
+ * DatePicker Component
+ * @since v1.0.0 
+ * @author [Anthony Freda](https://github.com/Afreda)
+ */
+const Button = () => {
+  return <Wrapper>DatePicker</Wrapper>
+}
+
+// Types
+
+// Styled Components
+const Wrapper = styled.div``
+
+export default Button
